e7c1f3efc51f54b44f7a28f19ef661c7978ed4b8
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2018-09-08 21:30:29 +0200

    Add simple connected component

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-import {Module} from 'cerebral';
-import * as sequences from './sequences';
+import { Module } from "cerebral";
+import * as sequences from "./sequences";
 
 // this is the main module of the app
 // other modules can be added to it
 // in the module key,
 // we can also add signals
 const appModule = Module({
-    state: {
-        hasName: false,
-    },
-    signals: {
-        setName: sequences.setName
-    }
-})
+  state: {
+    hasName: false,
+    name: ""
+  },
+  signals: {
+    setName: sequences.setName
+  }
+});
 
 export default appModule;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Container } from "@cerebral/react";
+import { Container, connect } from "@cerebral/react";
 import controller from "./controller";
+import { state, signal } from "cerebral/tags";
+
+// connected component
+const NameInput = connect(
+  {
+    value: state`name`,
+    setName: signal`setName`
+  },
+  ({ value, setName }) => (
+    <div>
+      <input
+        value={value}
+        onChange={evt => setName({ name: evt.target.value })}
+      />
+      <div>{value}</div>
+    </div>
+  )
+);
 
 ReactDOM.render(
   <Container controller={controller}>
-    <div>Hello</div>
+    <NameInput />
   </Container>,
   document.getElementById("root")
 );
